24b9c542f1fd7894ef7313888348b00cb5588e58
Lukasz  Czajkowski <lukaszczajkowski@sda-11-mbpro.lan>
2020-11-09 11:46:08 +0100

    Initial commit

---
+++ src/test/java/se/kth/sda8/devnews/devnews/DevNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kth.sda8.devnews.devnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
